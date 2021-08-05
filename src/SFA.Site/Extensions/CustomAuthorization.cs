ed79364440f76f319403425c25d7dfaa9c5bf3b2
GuilhermeMalaquias <silvaam.guilherme@gmail.com>
2021-08-05 20:51:56 -0300

    finishing the CustomAuthorization

--- src/SFA.Site/Extensions/CustomAuthorization.cs
+++ src/SFA.Site/Extensions/CustomAuthorization.cs
@@ -1,5 +1,10 @@
+using System;
 using System.Linq;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
 
 namespace SFA.Site.Extensions
 {
@@ -11,4 +16,39 @@ namespace SFA.Site.Extensions
                    context.User.Claims.Any(c => c.Type == claimName && c.Value.Contains(claimValue));
         }
     }
+    public class RequiredClaimFilter : IAuthorizationFilter
+    {
+        private readonly Claim _claim;
+
+        public RequiredClaimFilter(Claim claim)
+        {
+            _claim = claim;
+        }
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (!context.HttpContext.User.Identity.IsAuthenticated)
+            {
+                context.Result = new RedirectToRouteResult(new RouteValueDictionary
+                (new
+                {
+                    area = "Identity", page = "/Account/Login", ReturnUrl = context.HttpContext.Request.Path.ToString()
+                }));
+                return;
+            }
+
+            if (!CustomAuthorization.ValidationUserClaims(context.HttpContext, _claim.Type, _claim.Value))
+            {
+                context.Result = new ForbidResult();
+            }
+        }
+    }
+
+    public class ClaimsAuthorizationAttribute : TypeFilterAttribute
+    {
+        public ClaimsAuthorizationAttribute(string claimName, string claimValue) :
+            base(typeof(RequiredClaimFilter))
+        {
+            Arguments = new object[] { new Claim(claimName, claimValue)};
+        }
+    } 
 }
